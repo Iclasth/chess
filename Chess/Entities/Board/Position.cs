593a816f1dde61e35a56c386700bfbdadebbd829
iclasTH <iclasinprogramacao@gmail.com>
2026-08-12 15:33:17 -0300

    feat(Position): add position entitie

---
+++ Chess/Entities/Board/Position.cs
@@ -0,0 +1,19 @@
+namespace Chess.Entities.Board
+{
+    public class Position
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+
+        public Position(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+
+        public override string ToString()
+        {
+            return $"({Row}, {Column})";
+        }
+    }
+}
