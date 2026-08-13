0b7ee4efb36d6c2e8904143c218e48e602aab1a4
iclasTH <iclasinprogramacao@gmail.com>
2026-08-13 09:51:46 -0300

    feat: create king and rook entities

---
+++ Chess/Entities/King.cs
@@ -0,0 +1,10 @@
+﻿using Chess.Entities.Board;
+
+namespace Chess.Entities;
+
+public class King : Piece
+{
+    public King(ChessBoard board, Color color) : base(board, color)
+    {
+    }
+}
---
+++ Chess/Entities/Rook.cs
@@ -0,0 +1,8 @@
+﻿namespace Chess.Entities;
+
+public class Rook : Piece
+{
+    public Rook(ChessBoard board, Color color) : base(board, color)
+    {
+    }
+}
