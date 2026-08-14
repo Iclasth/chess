243c5d523753a2376046cf84543b27864295fd47
iclasTH <iclasinprogramacao@gmail.com>
2026-08-14 16:00:25 -0300

    feat: implement king and rook moviment logic
    create a new method in position entity to make easier create an instance of a piece position. then implement ifs and whiles to validate specific kind and rook moviments

--- Chess/Entities/Pieces/King.cs
+++ Chess/Entities/Pieces/King.cs
@@ -13,4 +13,75 @@ public class King : Piece
     {
         return "K";
     }
+
+    public bool CanMove(Position position)
+    {
+        Piece piece = Board.Piece(position);
+        return piece == null || piece.Color != Color;
+    }
+
+    public override bool[,] PossibleMoves()
+    {
+        bool[,] moves = new bool[Board.Ranks, Board.Columns];
+
+        Position position = new Position(0, 0);
+
+        // Up
+        position.SetValues(Position.Rank - 1, Position.Column);
+        if (Board.IsValidPosition(position) && CanMove(position))
+        {
+            moves[position.Rank, position.Column] = true;
+        }
+
+        // Down
+        position.SetValues(Position.Rank + 1, Position.Column);
+        if (Board.IsValidPosition(position) && CanMove(position))
+        {
+            moves[position.Rank, position.Column] = true;
+        }
+
+        // Left
+        position.SetValues(Position.Rank, Position.Column - 1);
+        if (Board.IsValidPosition(position) && CanMove(position))
+        {
+            moves[position.Rank, position.Column] = true;
+        }
+
+        // Right
+        position.SetValues(Position.Rank, Position.Column + 1);
+        if (Board.IsValidPosition(position) && CanMove(position))
+        {
+            moves[position.Rank, position.Column] = true;
+        }
+
+        // Up-Left
+        position.SetValues(Position.Rank - 1, Position.Column - 1);
+        if (Board.IsValidPosition(position) && CanMove(position))
+        {
+            moves[position.Rank, position.Column] = true;
+        }
+
+        // Up-Right
+        position.SetValues(Position.Rank - 1, Position.Column + 1);
+        if (Board.IsValidPosition(position) && CanMove(position))
+        {
+            moves[position.Rank, position.Column] = true;
+        }
+
+        // Down-Left
+        position.SetValues(Position.Rank + 1, Position.Column - 1);
+        if (Board.IsValidPosition(position) && CanMove(position))
+        {
+            moves[position.Rank, position.Column] = true;
+        }
+
+        // Down-Right
+        position.SetValues(Position.Rank + 1, Position.Column + 1);
+        if (Board.IsValidPosition(position) && CanMove(position))
+        {
+            moves[position.Rank, position.Column] = true;
+        }
+
+        return moves;
+    }
 }
--- Chess/Entities/Pieces/Rook.cs
+++ Chess/Entities/Pieces/Rook.cs
@@ -7,6 +7,68 @@ public class Rook : Piece
     {
     }
 
+    public bool CanMove(Position position)
+    {
+        Piece piece = Board.Piece(position);
+        return piece == null || piece.Color != Color;
+    }
+
+    public override bool[,] PossibleMoves()
+    {
+        bool[,] moves = new bool[Board.Ranks, Board.Columns];
+
+        Position position = new Position(0, 0);
+
+        // Up
+        position.SetValues(Position.Rank - 1, Position.Column);
+        while (Board.IsValidPosition(position) && CanMove(position))
+        {
+            moves[position.Rank, position.Column] = true;
+            if (Board.Piece(position) != null && Board.Piece(position).Color != Color)
+            {
+                break;
+            }
+            position.SetValues(position.Rank - 1, position.Column);
+        }
+
+        // Down
+        position.SetValues(Position.Rank + 1, Position.Column);
+        while (Board.IsValidPosition(position) && CanMove(position))
+        {
+            moves[position.Rank, position.Column] = true;
+            if (Board.Piece(position) != null && Board.Piece(position).Color != Color)
+            {
+                break;
+            }
+            position.SetValues(position.Rank + 1, position.Column);
+        }
+
+        // Left
+        position.SetValues(Position.Rank, Position.Column - 1);
+        while (Board.IsValidPosition(position) && CanMove(position))
+        {
+            moves[position.Rank, position.Column] = true;
+            if (Board.Piece(position) != null && Board.Piece(position).Color != Color)
+            {
+                break;
+            }
+            position.SetValues(position.Rank, position.Column - 1);
+        }
+
+        // Right
+        position.SetValues(Position.Rank, Position.Column + 1);
+        while (Board.IsValidPosition(position) && CanMove(position))
+        {
+            moves[position.Rank, position.Column] = true;
+            if (Board.Piece(position) != null && Board.Piece(position).Color != Color)
+            {
+                break;
+            }
+            position.SetValues(position.Rank, position.Column + 1);
+        }
+
+        return moves;
+    }
     public override string ToString()
     {
         return "R";
