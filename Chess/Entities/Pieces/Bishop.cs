953665956487b1c09698b620d763888cf58cb482
iclasTH <iclasinprogramacao@gmail.com>
2026-08-17 11:13:52 -0300

    feat: add Bishop, Knight, Pawn, Queen

---
+++ Chess/Entities/Pieces/Bishop.cs
@@ -0,0 +1,79 @@
+﻿using Chess.Entities.Board;
+using Chess.Entities.Enums;
+
+namespace Chess.Entities;
+
+public class Bishop : Piece
+{
+    public Bishop(ChessBoard board, Color color) : base(board, color)
+    {
+    }
+
+    public override string ToString()
+    {
+        return "B";
+    }
+
+    public bool CanMove(Position position)
+    {
+        Piece piece = Board.Piece(position);
+        return piece == null || piece.Color != Color;
+    }
+
+    public override bool[,] PossibleMoves()
+    {
+        bool[,] moves = new bool[Board.Ranks, Board.Columns];
+
+        Position position = new Position(0, 0);
+
+        // Up-Left
+        position.SetValues(Position.Rank - 1, Position.Column - 1);
+        while (Board.IsValidPosition(position) && CanMove(position))
+        {
+            moves[position.Rank, position.Column] = true;
+            if (Board.Piece(position) != null && Board.Piece(position).Color != Color)
+            {
+                break;
+            }
+            position.SetValues(position.Rank - 1, position.Column - 1);
+        }
+
+        // Up-Right
+        position.SetValues(Position.Rank - 1, Position.Column + 1);
+        while (Board.IsValidPosition(position) && CanMove(position))
+        {
+            moves[position.Rank, position.Column] = true;
+            if (Board.Piece(position) != null && Board.Piece(position).Color != Color)
+            {
+                break;
+            }
+            position.SetValues(position.Rank - 1, position.Column + 1);
+        }
+
+        // Down-Right
+        position.SetValues(Position.Rank + 1, Position.Column + 1);
+        while (Board.IsValidPosition(position) && CanMove(position))
+        {
+            moves[position.Rank, position.Column] = true;
+            if (Board.Piece(position) != null && Board.Piece(position).Color != Color)
+            {
+                break;
+            }
+            position.SetValues(position.Rank + 1, position.Column + 1);
+        }
+
+        // Down-Left
+        position.SetValues(Position.Rank + 1, Position.Column - 1);
+        while (Board.IsValidPosition(position) && CanMove(position))
+        {
+            moves[position.Rank, position.Column] = true;
+            if (Board.Piece(position) != null && Board.Piece(position).Color != Color)
+            {
+                break;
+            }
+            position.SetValues(position.Rank + 1, position.Column - 1);
+        }
+
+        return moves;
+    }
+}
---
+++ Chess/Entities/Pieces/Knight.cs
@@ -0,0 +1,59 @@
+﻿using Chess.Entities.Board;
+using Chess.Entities.Enums;
+
+namespace Chess.Entities;
+
+public class Knight : Piece
+{
+    public Knight(ChessBoard board, Color color) : base(board, color)
+    {
+    }
+
+    public override string ToString()
+    {
+        return "N";
+    }
+
+    public bool CanMove(Position position)
+    {
+        Piece piece = Board.Piece(position);
+        return piece == null || piece.Color != Color;
+    }
+
+    public override bool[,] PossibleMoves()
+    {
+        bool[,] moves = new bool[Board.Ranks, Board.Columns];
+
+        Position position = new Position(0, 0);
+
+        // Up-Left
+        position.SetValues(Position.Rank - 2, Position.Column - 1);
+        if (Board.IsValidPosition(position) && CanMove(position))
+        {
+            moves[position.Rank, position.Column] = true;
+        }
+
+        // Up-Right
+        position.SetValues(Position.Rank - 2, Position.Column + 1);
+        if (Board.IsValidPosition(position) && CanMove(position))
+        {
+            moves[position.Rank, position.Column] = true;
+        }
+
+        // Down-Right
+        position.SetValues(Position.Rank + 2, Position.Column + 1);
+        if (Board.IsValidPosition(position) && CanMove(position))
+        {
+            moves[position.Rank, position.Column] = true;
+        }
+
+        // Down-Left
+        position.SetValues(Position.Rank + 2, Position.Column - 1);
+        if (Board.IsValidPosition(position) && CanMove(position))
+        {
+            moves[position.Rank, position.Column] = true;
+        }
+
+        return moves;
+    }
+}
